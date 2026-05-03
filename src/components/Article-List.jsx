dbb54ad423663f19d051eb997e4bb134061be7db
Elvis Chege <elvischege98@gmail.com>
2026-05-03 20:45:55 +0300

    Finished coding

---
+++ src/components/Article-List.jsx
@@ -0,0 +1,11 @@
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  const articleElements = posts.map((post) => {
+    return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />;
+  });
+
+  return <main>{articleElements}</main>;
+}
+
+export default ArticleList;
